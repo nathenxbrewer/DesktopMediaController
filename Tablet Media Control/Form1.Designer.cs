3acc414d554c32a3b841b9311b519003bfa805e5
Nathen Brewer <nathenbrewer@gmail.com>
2020-06-02 11:35:27 -0400

    First Commit
    First Commit

---
+++ Tablet Media Control/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Tablet_Media_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.volumeSlider = new MetroFramework.Controls.MetroTrackBar();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.picMute = new System.Windows.Forms.PictureBox();
+            this.picPrev = new System.Windows.Forms.PictureBox();
+            this.picNext = new System.Windows.Forms.PictureBox();
+            this.picPlay = new System.Windows.Forms.PictureBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMute)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPrev)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNext)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // volumeSlider
+            // 
+            this.volumeSlider.BackColor = System.Drawing.Color.Transparent;
+            this.volumeSlider.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.volumeSlider.ForeColor = System.Drawing.Color.Cornsilk;
+            this.volumeSlider.Location = new System.Drawing.Point(179, 361);
+            this.volumeSlider.Name = "volumeSlider";
+            this.volumeSlider.Size = new System.Drawing.Size(451, 47);
+            this.volumeSlider.TabIndex = 3;
+            this.volumeSlider.Text = "metroTrackBar1";
+            this.volumeSlider.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.volumeSlider.ValueChanged += new System.EventHandler(this.volumeSlider_ValueChanged);
+            this.volumeSlider.Scroll += new System.Windows.Forms.ScrollEventHandler(this.metroTrackBar1_Scroll);
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = null;
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblVolume.Font = new System.Drawing.Font("Segoe UI", 26.25F);
+            this.lblVolume.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(147)))), ((int)(((byte)(147)))));
+            this.lblVolume.Location = new System.Drawing.Point(359, 413);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(80, 47);
+            this.lblVolume.TabIndex = 6;
+            this.lblVolume.Text = "100";
+            this.lblVolume.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblVolume.Click += new System.EventHandler(this.lblVolume_Click);
+            // 
+            // picMute
+            // 
+            this.picMute.Image = global::Tablet_Media_Control.Properties.Resources.volume_mute_solid;
+            this.picMute.Location = new System.Drawing.Point(737, 430);
+            this.picMute.Name = "picMute";
+            this.picMute.Size = new System.Drawing.Size(40, 40);
+            this.picMute.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMute.TabIndex = 5;
+            this.picMute.TabStop = false;
+            this.picMute.Click += new System.EventHandler(this.picMute_Click);
+            // 
+            // picPrev
+            // 
+            this.picPrev.Image = global::Tablet_Media_Control.Properties.Resources.step_backward_solid1;
+            this.picPrev.Location = new System.Drawing.Point(76, 153);
+            this.picPrev.Name = "picPrev";
+            this.picPrev.Size = new System.Drawing.Size(160, 160);
+            this.picPrev.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPrev.TabIndex = 2;
+            this.picPrev.TabStop = false;
+            this.picPrev.Click += new System.EventHandler(this.picPrev_Click);
+            // 
+            // picNext
+            // 
+            this.picNext.Image = global::Tablet_Media_Control.Properties.Resources.step_forward_solid1;
+            this.picNext.Location = new System.Drawing.Point(555, 153);
+            this.picNext.Name = "picNext";
+            this.picNext.Size = new System.Drawing.Size(160, 160);
+            this.picNext.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picNext.TabIndex = 1;
+            this.picNext.TabStop = false;
+            this.picNext.Click += new System.EventHandler(this.picNext_Click);
+            // 
+            // picPlay
+            // 
+            this.picPlay.Image = global::Tablet_Media_Control.Properties.Resources.play_circle_regular1;
+            this.picPlay.Location = new System.Drawing.Point(299, 133);
+            this.picPlay.Name = "picPlay";
+            this.picPlay.Size = new System.Drawing.Size(200, 200);
+            this.picPlay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlay.TabIndex = 0;
+            this.picPlay.TabStop = false;
+            this.picPlay.Click += new System.EventHandler(this.picPlay_Click);
+            this.picPlay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picPlay_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.picMute);
+            this.Controls.Add(this.volumeSlider);
+            this.Controls.Add(this.picPrev);
+            this.Controls.Add(this.picNext);
+            this.Controls.Add(this.picPlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "Mini Media Controller";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMute)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPrev)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNext)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPlay;
+        private System.Windows.Forms.PictureBox picNext;
+        private System.Windows.Forms.PictureBox picPrev;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private MetroFramework.Controls.MetroTrackBar volumeSlider;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private System.Windows.Forms.PictureBox picMute;
+        private System.Windows.Forms.Label lblVolume;
+    }
+}
+
---
+++ Tablet Media Control/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace Tablet_Media_Control
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+
+
+        bool isPlaying = true;
+        bool isMute;
+        double currentVolume = 0;
+        double sliderVolume = 0;
+
+ 
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            metroStyleManager1.Owner = this;
+
+            currentVolume = defaultPlaybackDevice.Volume;
+
+            lblVolume.Text = currentVolume.ToString();
+            volumeSlider.Value = Convert.ToInt32(currentVolume);
+
+
+            picPlay.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("play-circle-regular1");
+
+            defaultPlaybackDevice.Mute(false);
+            isMute = false;
+
+
+
+        }
+
+        private void metroTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        [DllImport("user32.dll")]
+        internal static extern void keybd_event(VirtualKeyCode bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        internal static extern uint MapVirtualKey(uint uCode, MapVirtualKeyType uMapType);
+
+        // https://docs.microsoft.com/en-gb/windows/desktop/inputdev/virtual-key-codes
+
+        internal enum VirtualKeyCode : byte
+        {
+            VK_MEDIA_PLAY_PAUSE = 0xB3,
+            VK_MEDIA_NEXT_TRACK = 0xB0,
+            VK_MEDIA_PREV_TRACK = 0xB1,
+            VK_VOLUME_MUTE = 0xAD
+
+    }
+
+        const uint KEYEVENTF_EXTENDEDKEY = 1;
+        const uint KEYEVENTF_KEYUP = 2;
+
+        internal enum MapVirtualKeyType : uint
+        {
+            MAPVK_VK_TO_VSC = 0,
+            MAPVK_VSC_TO_VK = 1,
+            MAPVK_VK_TO_CHAR = 2,
+            MAPVK_VSC_TO_VK_EX = 3,
+            MAPVK_VK_TO_VSC_EX = 4
+        }
+
+        public void PlayPause()
+        {
+            uint scanCode = 0x22;
+            // uint scanCode = MapVirtualKey((uint)VirtualKeyCode.VK_MEDIA_PLAY_PAUSE, MapVirtualKeyType.MAPVK_VK_TO_VSC);
+
+            keybd_event(VirtualKeyCode.VK_MEDIA_PLAY_PAUSE, (byte)scanCode, KEYEVENTF_EXTENDEDKEY | 0, 0);
+            // Thread.Sleep ?
+            keybd_event(VirtualKeyCode.VK_MEDIA_PLAY_PAUSE, (byte)scanCode, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
+        public void Next()
+        {
+            uint scanCode = 0x22;
+            // uint scanCode = MapVirtualKey((uint)VirtualKeyCode.VK_MEDIA_PLAY_PAUSE, MapVirtualKeyType.MAPVK_VK_TO_VSC);
+
+            keybd_event(VirtualKeyCode.VK_MEDIA_NEXT_TRACK, (byte)scanCode, KEYEVENTF_EXTENDEDKEY | 0, 0);
+            // Thread.Sleep ?
+            //keybd_event(VirtualKeyCode.VK_MEDIA_PLAY_PAUSE, (byte)scanCode, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
+        public void Previous()
+        {
+            uint scanCode = 0x22;
+            // uint scanCode = MapVirtualKey((uint)VirtualKeyCode.VK_MEDIA_PLAY_PAUSE, MapVirtualKeyType.MAPVK_VK_TO_VSC);
+
+            keybd_event(VirtualKeyCode.VK_MEDIA_PREV_TRACK, (byte)scanCode, KEYEVENTF_EXTENDEDKEY | 0, 0);
+            // Thread.Sleep ?
+            //keybd_event(VirtualKeyCode.VK_MEDIA_PLAY_PAUSE, (byte)scanCode, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
+        private void picPlay_Click(object sender, EventArgs e)
+        {
+
+            PlayPause();
+            //"Current Volume:" + defaultPlaybackDevice.Volume);
+        }
+
+        private void picNext_Click(object sender, EventArgs e)
+        {
+            Next();
+        }
+
+        private void picPrev_Click(object sender, EventArgs e)
+        {
+            Previous();
+        }
+
+        private void picPlay_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (isPlaying)
+            {
+                picPlay.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("pause-circle-regular1");
+                picPlay.Refresh();
+                isPlaying = false;
+            }
+            else if (!isPlaying)
+            {
+                picPlay.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("play-circle-regular1");
+                picPlay.Refresh();
+                isPlaying = true;
+            }
+
+
+        }
+
+        private void volumeSlider_ValueChanged(object sender, EventArgs e)
+        {
+            //defaultPlaybackDevice.Volume = 80;
+           
+
+            sliderVolume = volumeSlider.Value;
+            defaultPlaybackDevice.Volume = sliderVolume;
+            lblVolume.Text = sliderVolume.ToString();
+
+        }
+
+        private void lblVolume_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picMute_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (isMute)
+            {
+                picMute.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("volume-mute");
+                picMute.Refresh();
+                isMute = false;
+            }
+            else if (!isMute)
+            {
+                picMute.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("volume-mute-solid");
+                picMute.Refresh();
+                isMute = true;
+            }
+        }
+        private void Mute()
+        {
+            uint scanCode = 0x22;
+            keybd_event(VirtualKeyCode.VK_VOLUME_MUTE, (byte)scanCode, KEYEVENTF_EXTENDEDKEY | 0, 0);
+           
+
+        }
+
+        public void picMute_Click(object sender, EventArgs e)
+        {
+            if (!isMute)
+            {
+                Mute();
+                picMute.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("volume-mute-solid");
+                lblVolume.Enabled = false;
+                volumeSlider.Enabled = false;
+                isMute = true;
+            }
+            else
+            {
+                Mute();
+                picMute.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("volume-mute");
+                lblVolume.Enabled = true;
+                volumeSlider.Enabled = true;
+                isMute = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
